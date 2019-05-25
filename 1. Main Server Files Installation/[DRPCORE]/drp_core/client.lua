a8a5ce8e2e1625e939258212e42f4afb4430705b
Big-Yoda <43821187+Big-Yoda@users.noreply.github.com>
2019-05-25 12:41:05 +0200

    Update client.lua

--- 1. Main Server Files Installation/[DRPCORE]/drp_core/client.lua
+++ 1. Main Server Files Installation/[DRPCORE]/drp_core/client.lua
@@ -184,7 +184,38 @@ end)
 function AddTextEntry(key, value)
 	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
 end
----------------------------------------------------------------------------
+
+local config = {
+    ["TITLE"] = "DRP Framwork",
+    ["SUBTITLE"] = "Created by Darkzy",
+    ["MAP"] = "Map",
+    ["STATUS"] = "Status",
+    ["GAME"] = "Game",
+    ["INFO"] = "Info",
+    ["SETTINGS"] = "Settings",
+    ["R*EDITOR"] = "Rockstar Editor"
+}
+
 Citizen.CreateThread(function()
-  AddTextEntry('FE_THDR_GTAO', '~b~[DRP]Framework')
+    while true do
+    Citizen.Wait(0)
+        N_0xb9449845f73f5e9c("SHIFT_CORONA_DESC")
+        PushScaleformMovieFunctionParameterBool(true)
+        PopScaleformMovieFunction()
+        N_0xb9449845f73f5e9c("SET_HEADER_TITLE")
+        PushScaleformMovieFunctionParameterString(config["TITLE"])
+        PushScaleformMovieFunctionParameterBool(true)
+        PushScaleformMovieFunctionParameterString(config["SUBTITLE"])
+        PushScaleformMovieFunctionParameterBool(true)
+        PopScaleformMovieFunctionVoid()
+    end
+end)
+
+Citizen.CreateThread(function()
+    AddTextEntry('PM_SCR_MAP', config["MAP"])
+    AddTextEntry('PM_SCR_STA', config["STATUS"])
+    AddTextEntry('PM_SCR_GAM', config["GAME"])
+    AddTextEntry('PM_SCR_INF', config["INFO"])
+    AddTextEntry('PM_SCR_SET', config["SETTINGS"])
+    AddTextEntry('PM_SCR_RPL', config["R*EDITOR"])
 end)
